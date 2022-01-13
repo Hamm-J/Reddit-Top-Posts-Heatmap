ad8167513ea8cc774c169ec36021e6934bb531d1
Jason Hamm <jason.robert.hamm@gmail.com>
2022-01-12 22:58:31 -0500

    Connect comments to inspector

--- src/components/Inspector/Inspector.tsx
+++ src/components/Inspector/Inspector.tsx
@@ -4,7 +4,10 @@ import { InspectorContainer } from "./Inspector.styled";
 import { unixToDayHour } from "../../helpers/UTCConversions";
 
 const Inspector = () => {
-  const { selectedCell } = useContext<any>(FetcherContext);
+  const { selectedCell, comments } = useContext<any>(FetcherContext);
+  console.log("selectedcell");
+  // console.log(selectedCell[0].id);
+  // console.log(selectedCell);
   return (
     <InspectorContainer>
       <p>Inspector</p>
@@ -13,6 +16,18 @@ const Inspector = () => {
           <div>{post.permalink}</div>
           <div>
             <b>{unixToDayHour(post.date)}</b>
+          </div>
+          <div>
+            <b>{post.id}</b>
+          </div>
+          <div>
+            <p>comments stuff:</p>
+            {comments[selectedCell[0].id].map((val: any, valIdx: any) => (
+              <div key={valIdx}>
+                <div>{val.id}</div>
+                <div>{val.body}</div>
+              </div>
+            ))}
           </div>
         </div>
       ))}
