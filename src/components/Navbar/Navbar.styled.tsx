6c646d4721b4e32fd388d91ab0f3e6f2dd1ce801
Jason Hamm <jason.robert.hamm@gmail.com>
2022-01-25 23:12:17 -0500

    Import redditLogo image into Navbar
    -Add `redditLogo` image to `Navbar`

--- src/components/Navbar/Navbar.styled.tsx
+++ src/components/Navbar/Navbar.styled.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
 export const NavbarContainer = styled.div`
-  background-color: blue;
+  /* background-color: blue; */
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { NavbarContainer } from "./Navbar.styled";
 import Login from "../Login/Login";
+import redditLogo from "../../images/redditLogo.png";
 
 const Navbar = () => {
   return (
     <NavbarContainer>
+      <img src={redditLogo} alt="reddit logo" height="50px" width="50px" />
       <Login></Login>
     </NavbarContainer>
   );
