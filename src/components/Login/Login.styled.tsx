b03dfc408e4fd527d7510eb6034b64f1d6cab3b1
Jason Hamm <jason.robert.hamm@gmail.com>
2022-01-26 22:22:56 -0500

    Add temp navbar styling

--- src/components/Login/Login.styled.tsx
+++ src/components/Login/Login.styled.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const LoginContainer = styled.div`
-  background-color: red;
   display: flex;
   justify-content: right;
   align-items: center;
--- src/components/Navbar/Navbar.styled.tsx
+++ src/components/Navbar/Navbar.styled.tsx
@@ -2,9 +2,7 @@ import styled from "styled-components";
 
 export const NavbarContainer = styled.div`
   background-color: gray;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  padding-left: 5px;
+  padding: 5px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
