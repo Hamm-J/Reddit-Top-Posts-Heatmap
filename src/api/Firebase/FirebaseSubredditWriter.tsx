1a36261533f079a428112c8b1ecbb539b68f00d1
Jason Hamm <jason.robert.hamm@gmail.com>
2022-02-13 13:18:54 -0500

    Create new FirebaseSubredditWriter
    - Create a new `FirebaseSubredditWriter` which will be used to decouple
      the UI components from the logic via `useFirebaseWriter`
    - Temporarily renamed old version to `_FirebaseSubredditWriter`.

--- src/api/Firebase/FirebaseSubredditWriter.tsx
+++ src/api/Firebase/FirebaseSubredditWriter.tsx
@@ -1,11 +1,4 @@
-import { useContext } from "react";
-import { FetcherContext } from "../../contexts/FetcherContext";
-import { doc, setDoc } from "firebase/firestore";
-import { db, auth } from "../../firebase-config";
-import {
-  newDateToUTC,
-  unixToCalendarDateTime,
-} from "../../helpers/UTCConversions";
+import React from "react";
 import Button from "../../components/common/Button/Button";
 
 interface IFirebaseSubredditWriter {
@@ -16,72 +9,17 @@ const FirebaseSubredditWriter = ({
   posts,
   postCounts,
 }: IFirebaseSubredditWriter) => {
-  const { comments, subreddit, user } = useContext<any>(FetcherContext);
-
-  const createSubreddit = async () => {
-    if (auth.currentUser === null) {
-      return alert("You must sign in to save the data!");
-    }
-
-    const saveTime = newDateToUTC(new Date());
-
-    // package the posts, postCounts, and comments with `docType` property for
-    // query filtering
-    const postsPackaged = {
-      docType: "posts",
-      data: posts,
-    };
-
-    const postCountsPackaged = {
-      docType: "postCounts",
-      data: postCounts,
-    };
-
-    // const commentsPackaged = {
-    //   docType: "comments",
-    //   data: comments,
-    // };
-
-    const postsSnapshotcollectionRef = doc(
-      db,
-      user.uid,
-      `${subreddit}_posts_${saveTime}`
-    );
-
-    const postCountsSnapshotCollectionRef = doc(
-      db,
-      user.uid,
-      `${subreddit}_postCounts_${saveTime}`
-    );
-
-    // const commentsSnapshotCollectionRef = doc(
-    //   db,
-    //   user.uid,
-    //   `${subreddit}_comments_${saveTime}`
-    // );
-
-    alert(
-      `Saved Subreddit: "${subreddit}" data at ${unixToCalendarDateTime(
-        saveTime
-      )}`
-    );
-    setDoc(postsSnapshotcollectionRef, postsPackaged, { merge: true });
-    setDoc(postCountsSnapshotCollectionRef, postCountsPackaged, {
-      merge: true,
-    });
-    // setDoc(commentsSnapshotCollectionRef, commentsPackaged, { merge: true });
+  const saveSubreddit = async () => {
+    console.log(posts);
+    console.log(postCounts);
   };
-
   return (
-    <>
-      {/* <button onClick={createSubreddit}>Save Post Data</button> */}
-      <Button
-        label="Save Subreddit Data?"
-        onClick={createSubreddit}
-        remFontSize={1.2}
-        backgroundColor="orange"
-      ></Button>
-    </>
+    <Button
+      label="Save Subreddit Data?"
+      onClick={saveSubreddit}
+      remFontSize={1.2}
+      backgroundColor="orange"
+    ></Button>
   );
 };
 
--- src/api/Firebase/useFirebaseWriter.ts
+++ src/api/Firebase/useFirebaseWriter.ts
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { FetcherContext } from "../../contexts/FetcherContext";
 import { doc, setDoc } from "firebase/firestore";
 import { db, auth } from "../../firebase-config";
 import {
@@ -5,6 +7,60 @@ import {
   unixToCalendarDateTime,
 } from "../../helpers/UTCConversions";
 
-const useFirebaseWriter = (posts: {}, postCounts: any) => {};
+const useFirebaseWriter = (posts: {}, postCounts: any) => {
+  const { subreddit, user } = useContext<any>(FetcherContext);
+
+  if (auth.currentUser === null) {
+    return alert("You must sign in to save the data!");
+  }
+
+  const saveTime = newDateToUTC(new Date());
+
+  // package the posts, postCounts, and comments with `docType` property for
+  // query filtering
+  const postsPackaged = {
+    docType: "posts",
+    data: posts,
+  };
+
+  const postCountsPackaged = {
+    docType: "postCounts",
+    data: postCounts,
+  };
+
+  // const commentsPackaged = {
+  //   docType: "comments",
+  //   data: comments,
+  // };
+
+  const postsSnapshotcollectionRef = doc(
+    db,
+    user.uid,
+    `${subreddit}_posts_${saveTime}`
+  );
+
+  const postCountsSnapshotCollectionRef = doc(
+    db,
+    user.uid,
+    `${subreddit}_postCounts_${saveTime}`
+  );
+
+  // const commentsSnapshotCollectionRef = doc(
+  //   db,
+  //   user.uid,
+  //   `${subreddit}_comments_${saveTime}`
+  // );
+
+  alert(
+    `Saved Subreddit: "${subreddit}" data at ${unixToCalendarDateTime(
+      saveTime
+    )}`
+  );
+  setDoc(postsSnapshotcollectionRef, postsPackaged, { merge: true });
+  setDoc(postCountsSnapshotCollectionRef, postCountsPackaged, {
+    merge: true,
+  });
+  // setDoc(commentsSnapshotCollectionRef, commentsPackaged, { merge: true });
+};
 
 export default useFirebaseWriter;
