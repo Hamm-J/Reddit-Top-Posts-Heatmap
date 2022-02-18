0010fc800d091cc98245361183139cb4d605b4a2
Jason Hamm <jason.robert.hamm@gmail.com>
2022-02-17 22:38:13 -0500

    Remove UserDashboard login screen redirect
    - I have disabled disabling the login modal when clicking
      `UserDashboard` without being signed in.
    - I am just going to hide the home and userdashboard navbar links until
      logged in

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,37 +1,11 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import Home from "./Home";
-import RegisterScreen from "../components/RegisterScreen/RegisterScreen";
 import { FetcherContext } from "../contexts/FetcherContext";
 
 const ProtectedRoute = () => {
-  const { user, isOpen, setIsOpen } = useContext<any>(FetcherContext);
+  const { user } = useContext<any>(FetcherContext);
 
-  const [protect, setProtect] = useState(true);
-
-  // If the user is logged in, turn off the protection for the user dashboard and
-  // continue to not show the register modal.
-  // If the user is not logged in, protect the user dashboard and show the
-  // register screen
-
-  useEffect(() => {
-    if (user) {
-      setProtect(false);
-      setIsOpen(false);
-    } else {
-      // pass
-      setIsOpen(true);
-    }
-  }, [user]);
-
-  return protect ? (
-    <>
-      <Navigate replace to="/" />
-      <RegisterScreen open={isOpen} onClose={() => setIsOpen(false)} />
-    </>
-  ) : (
-    <Outlet />
-  );
+  return user ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;
