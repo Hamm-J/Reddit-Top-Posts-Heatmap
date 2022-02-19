9f72a0f8cac7ddff25c602a49387433f1c0870b6
Jason Hamm <jason.robert.hamm@gmail.com>
2022-02-19 14:19:15 -0500

    Only show Home and UserDashboard links on loggedin

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,8 +19,12 @@ const Navbar = () => {
         <ImgAnchor href="https://www.reddit.com/" target="_blank">
           <img src={redditLogo} alt="reddit logo" height="50px" width="50px" />
         </ImgAnchor>
-        <NavbarLink to="/">Home</NavbarLink>
-        <NavbarLink to="/user_dashboard">User Dashboard</NavbarLink>
+        {user && (
+          <>
+            <NavbarLink to="/">Home</NavbarLink>
+            <NavbarLink to="/user_dashboard">User Dashboard</NavbarLink>
+          </>
+        )}
       </LinkWrapper>
       <LoginWrapper>
         <Login></Login>
