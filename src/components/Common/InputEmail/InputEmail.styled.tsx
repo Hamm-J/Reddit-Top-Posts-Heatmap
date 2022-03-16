4153660f29d9d534b3cbdabc4f4b23dabe9f8502
Jason Hamm <jason.robert.hamm@gmail.com>
2022-03-15 22:57:58 -0400

    Add padding to InputEmail

--- src/components/Common/InputEmail/InputEmail.styled.tsx
+++ src/components/Common/InputEmail/InputEmail.styled.tsx
@@ -9,4 +9,9 @@ export const InputField = styled.input.attrs({ type: "email" })<IInputField>`
   font-size: ${(props) => props.remFontSize}rem;
   border-width: ${(props) => props.borderThickness};
   border-color: black;
+  outline: none;
+  padding-left: ${(props) => props.remFontSize / 4}rem;
+  padding-right: ${(props) => props.remFontSize / 4}rem;
+  padding-top: ${(props) => props.remFontSize / 8}rem;
+  padding-bottom: ${(props) => props.remFontSize / 8}rem;
 `;
